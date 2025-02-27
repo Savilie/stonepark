d60753b5469496ba3e4330b9712c9dd052ba8215
Arthur <arturshabanyants@yandex.ru>
2025-02-27 15:17:04 +0300

    landing_first_section

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import HomePage from './pages/HomePage';
+import { BrowserRouter, Routes, Route } from "react-router";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage/>} />
+        </Routes>
+      </BrowserRouter>
   </StrictMode>,
 )
