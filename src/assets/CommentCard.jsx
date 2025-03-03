1b80958024af5880465a1404a5e0be89cdc997dd
Arthur <arturshabanyants@yandex.ru>
2025-03-03 15:07:39 +0300

    finished_dom

--- src/assets/CommentCard.jsx
+++ src/assets/CommentCard.jsx
@@ -1,11 +1,14 @@
-export default function CommentCard(comment){
-    return(
-        <>
-        <article className=" bg-white px-[1vw] py-[1vh]">
-            <div className=" flex">
-                <img src="" alt="картинка" />
-            </div>
-        </article>
-        </>
-    )
+export default function CommentCard(obj) {
+  const review = obj.obj;
+  console.log(review);
+
+  return (
+    <article className=" bg-[#F8F3ED] px-[1vw] py-[1vh] w-[32%] rounded-md shadow-md">
+      <div className="">
+        <img src={review.reviewer_image} alt="Фото" />
+      </div>
+      <p className=" text-[#7BA35A]">{review.reviewer_name}</p>
+      <p>{review.review_text}</p>
+    </article>
+  );
 }
